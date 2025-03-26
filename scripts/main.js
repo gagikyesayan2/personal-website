c124fc4160ba4b4a00e9532ad909ba917182b0c3
gagikyesayan2 <gagikyesayan2091@gmail.com>
2025-03-26 17:59:24 +0100

    initial commit: set up portfolio website

---
+++ scripts/main.js
@@ -0,0 +1,28 @@
+window.addEventListener('scroll',function(){
+    const header = document.querySelector('header');
+    const logo = document.querySelector('.logo');
+    logo.classList.toggle('sticky',window.scrollY );
+    header.classList.toggle('sticky',window.scrollY );
+});
+       
+function toggleMenu() {
+    let menuToggle = document.querySelector('.toggle');
+    let menu = document.querySelector('.menu');
+    const logo = document.querySelector('.logo');
+    menuToggle.classList.toggle('active');
+    logo.classList.toggle('active');
+    menu.classList.toggle('active');
+
+}
+
+
+function openExternalLinks(){
+    const linkedin = document.querySelector('.linkedin-icon');
+    const linkedinUrl = "https://www.linkedin.com/in/gagik-yesayan-a40951208/";
+    linkedin.onclick = () => window.open(linkedinUrl,'_blank');
+
+    const github = document.querySelector('.github-icon');
+    const githubUrl = "https://github.com/gagikyesayan2";
+    github.onclick = () => window.open(githubUrl,'_blank');
+}
+openExternalLinks();
