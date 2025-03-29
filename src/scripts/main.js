d43d84cd5974ab1839676fd14f9e5713e3db542a
gagikyesayan2 <gagikyesayan2091@gmail.com>
2025-03-29 16:42:03 +0100

    header changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,43 @@
+let scrollTimer;
+
 window.addEventListener('scroll',function(){
     const header = document.querySelector('header');
     const logo = document.querySelector('.logo');
+    const toggle = document.querySelector('.toggle');
+
     logo.classList.toggle('sticky',window.scrollY );
     header.classList.toggle('sticky',window.scrollY );
+    logo.style.opacity = 1;
+    toggle.style.opacity = 1;
+
+    
+
+    clearTimeout(header,toggle,logo);
+
+    scrollTimer = setTimeout(function () {
+        console.log("Scrolling has stopped!");
+        if(window.scrollY > 400){
+
+            onScrollEnd(header);
+        }
+        
+    }, 3220); 
 });
+
+function onScrollEnd(header) {
+    header.classList.remove('sticky');
+    header.style.transition = "1s";
+    const logo = document.querySelector('.logo');
+    const toggle = document.querySelector('.toggle');
+    toggle.style.opacity = 0;
+    logo.style.opacity = 0;
+}
        
 function toggleMenu() {
     let menuToggle = document.querySelector('.toggle');
     let menu = document.querySelector('.menu');
     const logo = document.querySelector('.logo');
+
     menuToggle.classList.toggle('active');
     logo.classList.toggle('active');
     menu.classList.toggle('active');
